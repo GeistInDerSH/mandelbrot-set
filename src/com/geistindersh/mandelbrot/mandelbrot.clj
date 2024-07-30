dac9d81032731311d384c5fbb084901de9ab0268
GeistInDerSH <geistindersh@gmail.com>
2024-07-30 11:01:02 -0700

    Performance Improvement for sequential-create-buffer
    Improve the performance of the sequential mandelbrot buffer creation so that it's faster than the existing parallel one.

--- src/com/geistindersh/mandelbrot/mandelbrot.clj
+++ src/com/geistindersh/mandelbrot/mandelbrot.clj
@@ -1,25 +1,21 @@
-(ns com.geistindersh.mandelbrot.mandelbrot
-  (:require
-    [clojure.math :as math]))
+(ns com.geistindersh.mandelbrot.mandelbrot)
 
 (defn mandelbrot
   "Calculate the mandelbrot value, based on the given constants"
   {:added "0.1.1"}
   [cx cy limit]
   (loop [i  (int 0)
-         xn (double 0.0)
-         yn (double 0.0)]
-    (if (and (< (+ (math/pow xn 2)
-                   (math/pow yn 2))
-                4)
-             (< i limit))
-      (let [x-temp (- (* xn xn)
-                      (* yn yn))
-            y-temp (* 2 xn yn)]
+         xn (float 0.0)
+         yn (float 0.0)]
+    (let [temp (float (* xn yn))
+          xn2  (float (* xn xn))
+          yn2  (float (* yn yn))]
+      (if (or (> (+ xn2 yn2) 16.0)
+              (>= i limit))
+        i
         (recur (inc i)
-               (+ cx x-temp)
-               (+ cy y-temp)))
-      i)))
+               (+ (- xn2 yn2) cx)
+               (+ temp temp cy))))))
 
 (defn- parallel-create-buffer
   "Generate a 1-D array of size x-res * y-res, and calculate the mandelbrot
@@ -44,26 +40,25 @@
 
 (defn- sequential-create-buffer
   "Generate a 1-D array of size x-res * y-res, and calculate the mandelbrot
-   value for each entry. Each 'row' is calculated sequentially"
+   value for each entry. This is done in parallel, with each 'x row' run
+   independently."
   {:added "0.1.1"}
   [options]
-  (let [{:keys [x-min y-min y-res x-res x-delta y-delta limit]} options
-        arr (int-array (* x-res y-res) 0)]
-    (doseq [y (range 0 y-res)
-            x (range 0 x-res)]
-      (let [cx    (+ (* x x-delta)
-                     x-min)
-            cy    (+ (* y y-delta)
-                     y-min)
-            index (+ (* y x-res) x)]
-        (aset arr index (mandelbrot cx cy limit))))
-    arr))
+  (let [{:keys [x-min y-min y-res x-res x-delta y-delta limit]} options]
+    (->> (for [x (range 0 x-res)
+               y (range 0 y-res)]
+           [x y])
+         (map (fn [[x y]]
+                (let [cx (float (+ (* x x-delta) x-min))
+                      cy (float (+ (* y y-delta) y-min))]
+                  (mandelbrot cx cy limit))))
+         (int-array))))
 
 (defn create-buffer
   "Generate a 1-D array of size x-res * y-res, and calculate the mandelbrot
    value for each entry."
   {:added "0.1.1"}
-  ([options] (create-buffer options true))
+  ([options] (create-buffer options false))
   ([options parallel?]
    (if parallel?
      (parallel-create-buffer options)
