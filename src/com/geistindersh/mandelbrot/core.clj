b8440762db51a39f9b944fed51ba61e3374f8de6
GeistInDerSH <geistindersh@gmail.com>
2024-07-30 12:04:54 -0700

    Time Buffer Creation
    Generate a mandelbrot buffer and time how long it takes

--- src/com/geistindersh/mandelbrot/core.clj
+++ src/com/geistindersh/mandelbrot/core.clj
@@ -1,5 +1,9 @@
 (ns com.geistindersh.mandelbrot.core
+  (:require [com.geistindersh.mandelbrot.mandelbrot :as mandelbrot]
+            [com.geistindersh.mandelbrot.options :as opt])
   (:gen-class))
 
-(defn -main [& args]
-  (println "Hello World"))
+(defn -main [& _]
+  (let [option (opt/make-options -1.0 1.0 15000 -1.0 1.0 15000)]
+    (time
+      (mandelbrot/create-buffer option false))))
