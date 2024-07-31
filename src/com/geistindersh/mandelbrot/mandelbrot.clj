0a11d31ba6530cf80670ac533672e6283a7c5e15
GeistInDerSH <GeistInDerSH@gmail.com>
2024-07-31 12:36:38 -0700

    Add create-colors and create-bitmap-byte-buffer
    Create a function for getting the colors to use in the image, based on the mandelbrot set and the ColorMap.
    Also create a function that will generate the byte array for use when making a bitmap image that can be converted to another image type

--- src/com/geistindersh/mandelbrot/mandelbrot.clj
+++ src/com/geistindersh/mandelbrot/mandelbrot.clj
@@ -1,4 +1,10 @@
-(ns com.geistindersh.mandelbrot.mandelbrot)
+(ns com.geistindersh.mandelbrot.mandelbrot
+  (:require
+    [clojure.math :as math]
+    [com.geistindersh.mandelbrot.color-map :as color])
+  (:import
+    (io.github.humbleui.skija ColorType)
+    (java.awt Color)))
 
 (defn mandelbrot
   "Calculate the mandelbrot value, based on the given constants"
@@ -31,3 +37,41 @@
               :let [cx (float (+ (* x x-delta) x-min))]]
         (aset-int arr (+ base x) (mandelbrot cx cy limit))))
     arr))
+
+(defn get-colors
+  "Get the colors that will be in the image"
+  {:added "0.2.4"}
+  [color-map options]
+  (let [{:keys [limit]} options
+        buffer        (create-buffer options)
+        lower-bound   (apply min buffer)
+        upper-bound   (max (inc (apply max buffer))
+                           limit)
+        dist          (- upper-bound lower-bound)
+        color-options (->> (range dist)
+                           (map #(math/pow (/ % dist) 0.5))
+                           (mapv #(color/get-at color-map %)))]
+    (->> buffer
+         (map (fn [val]
+                (if (>= val limit)
+                  Color/BLACK
+                  (get color-options (- val lower-bound)))))
+         (filter some?)
+         (vec))))
+
+(defn create-bitmap-byte-buffer
+  "Create a byte-array mapping to the pixel color values for the mandelbrot image.
+   The pixels in the buffer is allocated for RGBA 8888 images."
+  {:added "0.2.4"}
+  [options color-map]
+  (let [{:keys [x-res y-res]} options
+        color-buffer    (get-colors color-map options)
+        bytes-per-pixel (.getBytesPerPixel ColorType/RGBA_8888)
+        byte-buffer     (byte-array (* x-res y-res bytes-per-pixel) (byte 0))]
+    (doseq [[i color] (map-indexed color-buffer)
+            :let [base (* i bytes-per-pixel)]]
+      (aset-byte byte-buffer (+ 0 base) (unchecked-byte (* 255 (.getRed color))))
+      (aset-byte byte-buffer (+ 1 base) (unchecked-byte (* 255 (.getGreen color))))
+      (aset-byte byte-buffer (+ 2 base) (unchecked-byte (* 255 (.getBlue color))))
+      (aset-byte byte-buffer (+ 3 base) (unchecked-byte 255)))
+    byte-buffer))
