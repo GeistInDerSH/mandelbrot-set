ee96c0daceec14d104c61819b796ee16d8489d43
GeistInDerSH <GeistInDerSH@gmail.com>
2024-07-31 18:40:41 -0700

    Use Transducers to Generate Buffers Internally
    Avoid using int-array/byte-arrays directly and mutating by side effect. Instead, use function composition with transducers and then coerce the value into the correct type.

--- src/com/geistindersh/mandelbrot/mandelbrot.clj
+++ src/com/geistindersh/mandelbrot/mandelbrot.clj
@@ -3,7 +3,6 @@
     [clojure.math :as math]
     [com.geistindersh.mandelbrot.color-map :as color])
   (:import
-    (io.github.humbleui.skija ColorType)
     (java.awt Color)))
 
 (defn mandelbrot
@@ -29,48 +28,50 @@
   {:added "0.1.1"}
   [options]
   (let [{:keys [x-min y-min y-res x-res x-delta y-delta limit]} options
-        arr (int-array (* y-res x-res) 0)]
-    (doseq [y (range 0 y-res)
-            :let [base (int (* y x-res))
-                  cy   (float (+ (* y y-delta) y-min))]]
-      (doseq [x (range 0 x-res)
-              :let [cx (float (+ (* x x-delta) x-min))]]
-        (aset-int arr (+ base x) (mandelbrot cx cy limit))))
-    arr))
-
-(defn get-colors
-  "Get the colors that will be in the image"
-  {:added "0.2.4"}
-  [color-map options]
-  (let [{:keys [limit]} options
-        buffer        (create-buffer options)
-        lower-bound   (apply min buffer)
-        upper-bound   (max (inc (apply max buffer))
-                           limit)
-        dist          (- upper-bound lower-bound)
-        color-options (->> (range dist)
-                           (map #(math/pow (/ % dist) 0.5))
-                           (mapv #(color/get-at color-map %)))]
-    (->> buffer
-         (map (fn [val]
-                (if (>= val limit)
-                  Color/BLACK
-                  (get color-options (- val lower-bound)))))
-         (filter some?))))
+        y-vec (mapv (comp
+                      (partial + y-min)
+                      (partial * y-delta)
+                      float)
+                    (range y-res))
+        x-vec (mapv (comp
+                      (partial + x-min)
+                      (partial * x-delta)
+                      float)
+                    (range x-res))]
+    (into []
+          (comp
+            (map (fn [cy]
+                   (mapv #(mandelbrot % cy limit) x-vec)))
+            cat)
+          y-vec)))
 
 (defn create-bitmap-byte-buffer
   "Create a byte-array mapping to the pixel color values for the mandelbrot image.
    The pixels in the buffer is allocated for RGBA 8888 images."
   {:added "0.2.4"}
   [options color-map]
-  (let [{:keys [x-res y-res]} options
-        color-buffer    (get-colors color-map options)
-        bytes-per-pixel (.getBytesPerPixel ColorType/RGBA_8888)
-        byte-buffer     (byte-array (* x-res y-res bytes-per-pixel) (byte 0))]
-    (doseq [[i color] (map-indexed vector color-buffer)
-            :let [base (* i bytes-per-pixel)]]
-      (aset-byte byte-buffer (+ 0 base) (unchecked-byte (* 255 (.getRed color))))
-      (aset-byte byte-buffer (+ 1 base) (unchecked-byte (* 255 (.getGreen color))))
-      (aset-byte byte-buffer (+ 2 base) (unchecked-byte (* 255 (.getBlue color))))
-      (aset-byte byte-buffer (+ 3 base) (unchecked-byte 255)))
-    byte-buffer))
+  (let [{:keys [limit]} options
+        buffer        (create-buffer options)
+        lower-bound   (apply min buffer)
+        upper-bound   (inc (apply max buffer))
+        dist          (- upper-bound lower-bound)
+        color-options (into []
+                            (comp
+                              (map #(math/pow (/ % dist) 0.5))
+                              (map #(color/get-at color-map %)))
+                            (range dist))]
+    (->> buffer
+         (into []
+               (comp
+                 (map (fn [val]
+                        (if (>= val limit)
+                          Color/BLACK
+                          (get color-options (- val lower-bound)))))
+                 (filter some?)
+                 (map (fn [color]
+                        [(unchecked-byte (* 255 (.getRed color)))
+                         (unchecked-byte (* 255 (.getGreen color)))
+                         (unchecked-byte (* 255 (.getBlue color)))
+                         -1]))
+                 cat))
+         (byte-array))))
