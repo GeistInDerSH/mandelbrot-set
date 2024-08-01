5c09bf19b2f104093afc33e604c23be310a59ea1
GeistInDerSH <geistindersh@gmail.com>
2024-08-01 14:51:26 -0700

    Add Transducer for partial
    Add a function that can be used in a transducer to generate partial windows over sequences

---
+++ src/com/geistindersh/mandelbrot/utils.clj
@@ -0,0 +1,20 @@
+(ns com.geistindersh.mandelbrot.utils
+  (:import (java.util ArrayDeque)))
+
+(defn window
+  "A transducer compatible version of partial"
+  ([^long n] (window n 1))
+  ([^long n ^long step]
+   (fn [rf]
+     (let [a (ArrayDeque. n)]
+       (fn
+         ([] (rf))
+         ([result] (rf result))
+         ([result input]
+          (.add a input)
+          (if (= n (.size a))
+            (let [v (vec (.toArray a))]
+              (dotimes [_ step]
+                (.removeFirst a))
+              (rf result v))
+            result)))))))
