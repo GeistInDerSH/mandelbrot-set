2e3c3e03a974fb8859fcb89111aea5edc2c59f3c
Matthew Hill <GeistInDerSH@gmail.com>
2024-08-05 20:17:29 -0700

    Add Additional Build Config Options
    Allow for the environment to be set to enable/disable locals clearing.
    Add Built-By, Build-Machine, Release-Env information to Manifest

--- build.clj
+++ build.clj
@@ -22,20 +22,25 @@
 (defn clean [_]
   (build/delete {:path "target"}))
 
-(defn uber [_]
+(defn uber [{:keys [env]
+             :or   {env :debug}}]
   (clean nil)
   (build/copy-dir {:src-dirs   ["resources"]
                    :target-dir class-dir})
   (build/compile-clj {:basis        @basis
                       :bindings     {#'*warn-on-reflection* true}
-                      :compile-opts {:direct-linking true}
+                      :compile-opts {:direct-linking          true
+                                     :disable-locals-clearing (= env :debug)}
                       :ns-compile   '[com.geistindersh.mandelbrot.core]
                       :class-dir    class-dir})
   (build/uber {:class-dir class-dir
                :uber-file uber-file
-               :manifest  {"Revision" latest-revision
-                           "Built-On" (.format (LocalDateTime/now (ZoneId/of "America/Chicago"))
-                                               (DateTimeFormatter/ofPattern "yyyy/MM/dd HH:mm:SS"))}
+               :manifest  {"Revision"      latest-revision
+                           "Build-Date"    (.format (LocalDateTime/now (ZoneId/of "America/Chicago"))
+                                                    (DateTimeFormatter/ofPattern "yyyy/MM/dd HH:mm:SS"))
+                           "Built-By"      (System/getenv "USER")
+                           "Build-Machine" (System/getenv "HOSTNAME")
+                           "Release-Env"   (name env)}
                :basis     @basis
                :main      'com.geistindersh.mandelbrot.core}))
 
