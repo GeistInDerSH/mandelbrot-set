ae6a6d299561f1c62224558d08f39d40247229a8
Matthew Hill <GeistInDerSH@gmail.com>
2024-08-08 07:24:54 -0700

    Generate a Gradient
    Use the ColorMap to generate a gradient of colors for the image, not a map

--- src/com/geistindersh/mandelbrot/core.clj
+++ src/com/geistindersh/mandelbrot/core.clj
@@ -1,10 +1,19 @@
 (ns com.geistindersh.mandelbrot.core
-  (:require [com.geistindersh.mandelbrot.color-map :as color]
-            [com.geistindersh.mandelbrot.image :as img]
+  (:require [com.geistindersh.mandelbrot.color-map :as colors]
+            [com.geistindersh.mandelbrot.image :as image]
             [com.geistindersh.mandelbrot.options :as opt])
-  (:gen-class))
+  (:gen-class)
+  (:import (java.awt Color)))
 
 (defn -main [& _]
-  (let [option (opt/make-options -1.0 0.0 5000 0.0 1.0 5000)]
+  (let [option (opt/make-options -1.0 0.0 5000 0.0 1.0 5000)
+        color  (->> (colors/vec->ColorMap [(Color. (float 0) (float 0) (float 0.2))
+                                           Color/BLUE
+                                           Color/LIGHT_GRAY
+                                           (Color. (float 0.9) (float 0.7) (float 0.4))
+                                           Color/GRAY]
+                                          8)
+                    (:pairs)
+                    (mapv #'second))]
     (time
-      (img/create-mandelbrot-png "example/png/ppr.png" @color/pink-purple-100 option))))
+      (image/create-mandelbrot-png "example/png/smooth.png" option color))))
