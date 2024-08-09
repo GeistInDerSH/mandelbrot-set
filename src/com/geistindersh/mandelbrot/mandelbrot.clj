b7a51a890decdb8fa94928fdec344d8ae4796748
Matthew Hill <GeistInDerSH@gmail.com>
2024-08-09 06:45:17 -0700

    Re-add Parallel Generation
    Re-add a function that generates the image buffer in parallel

--- src/com/geistindersh/mandelbrot/mandelbrot.clj
+++ src/com/geistindersh/mandelbrot/mandelbrot.clj
@@ -65,7 +65,7 @@
                                  nu)))
         :else (double i)))))
 
-(defn create-bitmap-byte-buffer
+(defn- create-bitmap-byte-buffer-serial
   "Create a byte-array mapping to the pixel color values for the mandelbrot image.
    The pixels in the buffer is allocated for RGBA 8888 images."
   [options gradient]
@@ -74,7 +74,7 @@
         arr (ArrayList. (int (option/image-size options)))]
     (doseq [x (option/x-range options)
             y (option/y-range options)
-            :let [val       (double (mandelbrot x y limit))
+            :let [val       (double (mandelbrot-periodicity-checking x y limit))
                   alpha     (double (mod val 1))
                   index     (int (math/floor val))
                   ^Color c0 (nth colors index default-color)
@@ -84,3 +84,45 @@
       (.add arr (colors/linear-interpolation (.getBlue c0) (.getBlue c1) alpha))
       (.add arr -1))
     (bytes (byte-array (.toArray arr)))))
+
+(defn- create-mandelbrot-vals-parallel [options]
+  (let [{:keys [x-res y-res limit]} options
+        x-range (option/x-range options)
+        y-range (option/y-range options)
+        buff    (double-array (* x-res y-res))
+        tasks   (into []
+                      (map (fn [i]
+                             (let [x    (nth x-range i)
+                                   base (* i y-res)]
+                               (future
+                                 (doseq [j (range y-res)
+                                         :let [y (nth y-range j)]]
+                                   (aset-double buff (+ base j) (mandelbrot-periodicity-checking x y limit)))))))
+                      (range x-res))]
+    (doseq [task tasks] @task)
+    buff))
+
+(defn create-bitmap-byte-buffer-parallel
+  "Create a byte-array mapping to the pixel color values for the mandelbrot image.
+   The pixels in the buffer is allocated for RGBA 8888 images."
+  [options gradient]
+  (let [{:keys [colors default-color]} gradient
+        vals (create-mandelbrot-vals-parallel options)
+        arr  (ArrayList. (int (option/image-size options)))]
+    (doseq [val vals
+            :let [alpha     (double (mod val 1))
+                  index     (int (math/floor val))
+                  ^Color c0 (nth colors index default-color)
+                  ^Color c1 (nth colors (inc index) default-color)]]
+      (.add arr (colors/linear-interpolation (.getRed c0) (.getRed c1) alpha))
+      (.add arr (colors/linear-interpolation (.getGreen c0) (.getGreen c1) alpha))
+      (.add arr (colors/linear-interpolation (.getBlue c0) (.getBlue c1) alpha))
+      (.add arr -1))
+    (bytes (byte-array (.toArray arr)))))
+
+(defn create-bitmap-byte-buffer
+  ([options gradient] (create-bitmap-byte-buffer options gradient true))
+  ([options gradient parallel?]
+   (if parallel?
+     (create-bitmap-byte-buffer-parallel options gradient)
+     (create-bitmap-byte-buffer-serial options gradient))))
