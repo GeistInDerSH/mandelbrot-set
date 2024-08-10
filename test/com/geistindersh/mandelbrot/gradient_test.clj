0fa4cce6fabcbf81edccb5e3b5967bcf1fc58dd6
Matthew Hill <GeistInDerSH@gmail.com>
2024-08-10 09:27:56 -0700

    Add Test Coverage
    Add coverage for the pre-generated gradients

--- test/com/geistindersh/mandelbrot/gradient_test.clj
+++ test/com/geistindersh/mandelbrot/gradient_test.clj
@@ -1,7 +1,10 @@
 (ns com.geistindersh.mandelbrot.gradient-test
   (:require
     [clojure.test :refer [are deftest testing]]
-    [com.geistindersh.mandelbrot.gradient :refer [vec->Gradient]])
+    [com.geistindersh.mandelbrot.gradient :refer [lime-forest-gradient
+                                                  navy-gold-gradient
+                                                  neon-pink-ultramarine-gradient
+                                                  vec->Gradient]])
   (:import
     (java.awt Color)))
 
@@ -26,3 +29,15 @@
     (are [a b] (= a (:default-color b))
                Color/BLACK (vec->Gradient [Color/RED Color/BLUE])
                Color/PINK (vec->Gradient [Color/RED Color/BLUE] 128 Color/PINK))))
+
+(deftest pre-generated-gradients-test
+  (testing "The number of colors in the gradient are what we expect"
+    (are [a b] (= a (count (:colors b)))
+               128 @navy-gold-gradient
+               128 @lime-forest-gradient
+               128 @neon-pink-ultramarine-gradient))
+  (testing "The default color is what we expect"
+    (are [a b] (= a (:default-color b))
+               Color/BLACK @navy-gold-gradient
+               Color/BLACK @lime-forest-gradient
+               Color/BLACK @neon-pink-ultramarine-gradient)))
