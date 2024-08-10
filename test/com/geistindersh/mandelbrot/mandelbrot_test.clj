3450748f23c0b57fc19e2bcb9beb459c6ca5cb46
Matthew Hill <GeistInDerSH@gmail.com>
2024-08-10 09:30:26 -0700

    Add Test Coverage for Default Case
    Add test coverage for create-byte-buffer when no parallel? argument is given

--- test/com/geistindersh/mandelbrot/mandelbrot_test.clj
+++ test/com/geistindersh/mandelbrot/mandelbrot_test.clj
@@ -3,12 +3,12 @@
     [clojure.test :refer [are deftest is testing]]
     [com.geistindersh.mandelbrot.gradient :as gradient]
     [com.geistindersh.mandelbrot.mandelbrot :refer [create-byte-buffer mandelbrot-periodicity-checking]]
-    [com.geistindersh.mandelbrot.options :as options])
+    [com.geistindersh.mandelbrot.options :as opt])
   (:import
     (java.awt Color)
     (java.util Arrays)))
 
-(def ^:private testing-options (options/make-options))
+(def ^:private testing-options (opt/make-options -1.0 1.0 500 -1.0 1.0 500))
 (def ^:private testing-gradient (gradient/vec->Gradient [Color/RED Color/BLUE]))
 
 (deftest mandelbrot-test
@@ -24,6 +24,9 @@
                (mandelbrot-periodicity-checking 0.26726726726726735 0.18918918918918926 128) 128.0)))
 
 (deftest mandelbrot-create-byte-buffer-test
+  (testing "Bytes count matches expected"
+    (is (= (count (create-byte-buffer testing-options testing-gradient))
+           (opt/image-buffer-size testing-options))))
   (testing "Parallel buffers are the same"
     (is (Arrays/equals ^bytes (create-byte-buffer testing-options testing-gradient true)
                        ^bytes (create-byte-buffer testing-options testing-gradient true))))
